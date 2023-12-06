28dacab9848b89b28c38c4f428bb0410f3224c47
kacperczyk-dev <kacperczyk.dev@gmail.com>
2023-12-06 09:42:26 +0100

    Day05-Part2(wrong answer/incomplete)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.20"
+
 }
 
 sourceSets {
@@ -12,4 +13,13 @@ tasks {
     wrapper {
         gradleVersion = "8.5"
     }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0") // Add this line
 }
--- src/Day05.kt
+++ src/Day05.kt
@@ -1,9 +1,10 @@
-import java.math.BigInteger
-import kotlin.math.pow
+import java.util.*
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
 
-    data class ConversionRange(val dest: BigInteger, val source: BigInteger, val range: BigInteger)
+    data class ConversionRange(val dest: Long, val source: Long, val range: Long)
     data class ConversionMap(val order: Int, val name: String, val ranges: List<ConversionRange>)
 
 
@@ -14,28 +15,28 @@ fun main() {
             val lines = mapTxt.lines()
             val mapName = lines[0].dropLast(5)
             val ranges = lines.drop(1).map { line ->
-                val rangeValues = line.split(" ").map { it.toBigInteger() }
+                val rangeValues = line.split(" ").map { it.toLong() }
                 ConversionRange(
-                    rangeValues[0],
-                    rangeValues[1],
-                    rangeValues[2]
+                    rangeValues[0], rangeValues[1], rangeValues[2]
                 )
-            }
+            }.sortedBy { it.dest }
             ConversionMap(i, mapName, ranges)
         }
     }
 
-    fun processConversion(value: BigInteger, maps: List<ConversionMap>, order: Int): BigInteger {
+    fun processConversion(value: Long, maps: List<ConversionMap>, order: Int): Long {
         val currMap = maps.first { it.order == order }
         val dest = currMap.ranges.mapNotNull { range ->
-            if(value >= range.source && value < range.source + range.range) range.dest + (value - range.source) else null
+            if (value >= range.source && value < range.source + range.range) {
+                range.dest + (value - range.source)
+            } else null
         }
-        val nextVal =  if(dest.size == 1) dest[0] else value
-        return if(order == maps.size - 1) nextVal else processConversion(nextVal, maps, order + 1)
+        val nextVal = if (dest.size == 1) dest[0] else value
+        return if (order == maps.size - 1) nextVal else processConversion(nextVal, maps, order + 1)
     }
 
-    fun part1(input: List<String>): BigInteger {
-        val seeds = input[0].drop(6).trim().split(" ").map{ it.toBigInteger() }
+    fun part1(input: List<String>): Long {
+        val seeds = input[0].drop(6).trim().split(" ").map { it.toLong() }
         val maps = createMaps(input.drop(2))
         val locations = seeds.map { seed ->
             processConversion(seed, maps, 0)
@@ -43,15 +44,47 @@ fun main() {
         return locations.min()
     }
 
-    fun part2(input: List<String>): Int {
-
+    fun doRangesOverlap(range1: LongRange, range2: LongRange): Boolean {
+        return range1.first <= range2.last && range2.first <= range1.last
     }
 
-    // test if implementation meets criteria from the description, like:
+    fun processReverse(maps: List<ConversionMap>, order: Int): ConversionRange? {
+        for (currRange in maps[order].ranges) {
+            for (prevRange in maps[order - 1].ranges) {
+                if (doRangesOverlap(currRange.source until currRange.source + currRange.range, prevRange.dest until prevRange.dest + prevRange.range)) {
+                    if (order == 1) return prevRange
+                    return processReverse(maps, order - 1)
+                }
+            }
+        }
+        return null
+    }
+
+    fun part2(input: List<String>): Long {
+        var min: Long = -1L
+        val timeTaken = measureTimeMillis {
+            val x = input[0].drop(6).trim().split(" ").map { it.toLong() }
+            val maps = createMaps(input.drop(2))
+            val humToLoc = maps[6]
+            val seedRange = processReverse(maps, 6)!!
+            println(seedRange)
+            (seedRange.source until seedRange.source + seedRange.range).forEach { seed ->
+                println(seed)
+                val currVal = processConversion(seed, maps, 0)
+                if (min == -1L || currVal < min) {
+                    min = currVal
+                }
+            }
+        }
+
+        println("Time taken: ${timeTaken / 1000} s")
+        return min // 616794786
+
+        // test if implementation meets criteria from the description, like:
 //    val testInput = readInput("Day01_test")
 //    check(part1(testInput) == 1)
 
-    val input = readInput("Day05")
-    part1(input).println()
-    part2(input).println()
-}
+        val input = readInput("Day05")
+        part1(input).println()
+        part2(input).println()
+    }
